912368a31a9437f0414cb035bda15ecaf00ed3dd
Illia ABROSIMOV <illia.abrosimov@ubisoft.com>
2020-09-11 13:45:57 +0300

    deleted try catch from StartSessionCommandHandler

--- AuthServer/Mediator/Commands/StartSessionCommandHandler.cs
+++ AuthServer/Mediator/Commands/StartSessionCommandHandler.cs
@@ -17,28 +17,21 @@ namespace AuthServer.Mediator.Commands
 
         public async Task<SessionCommandResponce> Handle(StartSessionCommand command)
         {
-            try
-            {
-                // For now creating client and RO here is redundant
-                // Ideally, needs to be added contexts/repos for clients and ROs
-                Client client = new Client(int.Parse(command.ClientId), command.ClientSecret);
-                ResourceOwner resourceOwner = new ResourceOwner(int.Parse(command.ResourceOwnerId));
+            // For now creating client and RO here is redundant
+            // Ideally, needs to be added contexts/repos for clients and ROs
+            Client client = new Client(int.Parse(command.ClientId), command.ClientSecret);
+            ResourceOwner resourceOwner = new ResourceOwner(int.Parse(command.ResourceOwnerId));
 
-                Session session = new Session(client.Id, resourceOwner.Id, command.AccessParameters);
+            Session session = new Session(client.Id, resourceOwner.Id, command.AccessParameters);
 
-                if (SessionRepository.GetSessionById(session.Id) != null)
-                {
-                    return new SessionCommandResponce(false, null);
-                }
-
-                await SessionRepository.AddSession(session);
-
-                return new SessionCommandResponce(true, session?.Id);
-            }
-            catch (Exception)
+            if (SessionRepository.GetSessionById(session.Id) != null)
             {
                 return new SessionCommandResponce(false, null);
             }
+
+            await SessionRepository.AddSession(session);
+
+            return new SessionCommandResponce(true, session?.Id);
         }
     }
 }
